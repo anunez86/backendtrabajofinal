9f05b639c43b6cfae49f8eef070a6089f777b62d
anunez86 <102708449+anunez86@users.noreply.github.com>
2022-12-17 19:52:48 -0500

    Update articuloAnthonyController.java

--- src/main/java/com/example/trabajoFinal/Controller/articuloAnthonyController.java
+++ src/main/java/com/example/trabajoFinal/Controller/articuloAnthonyController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/articulo")
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://frontendtrabajofinal.azurewebsites.net/")
 public class articuloAnthonyController {
 
 
