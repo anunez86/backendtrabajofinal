d7f4f193f96e48b17b9fcabb7d7c4e40357a0aae
anunez86 <102708449+anunez86@users.noreply.github.com>
2022-12-17 19:53:51 -0500

    Update modeloNoemiController.java

--- src/main/java/com/example/trabajoFinal/Controller/modeloNoemiController.java
+++ src/main/java/com/example/trabajoFinal/Controller/modeloNoemiController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/modelo")
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://frontendtrabajofinal.azurewebsites.net/")
 public class modeloNoemiController {
 
 
